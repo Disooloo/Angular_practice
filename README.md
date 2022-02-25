996acf9cd2aed3222021714e46e26422c791114c
disooloo <disooloo@mail.ru>
2022-02-26 03:03:54 +0500

    todo

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+
 # AngularTask
+
+![img.png](img.png)
+
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import {FormsModule} from "@angular/forms";
+import {TodosFilperPipe} from "./service/todos.filper.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilperPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
